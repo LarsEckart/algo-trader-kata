6264d84b6a37d5d36d6b123b63e989363016e1fc
Lars Eckart <lars.eckart@gmail.com>
2020-05-30 01:31:11 +0300

    3h refactoring with Llewellyn Falco

--- src/main/java/com/turleylabs/algo/trader/kata/RefactorMeAlgorithm.java
+++ src/main/java/com/turleylabs/algo/trader/kata/RefactorMeAlgorithm.java
@@ -1,20 +1,19 @@
 package com.turleylabs.algo.trader.kata;
 
-import com.turleylabs.algo.trader.kata.framework.BaseAlgorithm;
-import com.turleylabs.algo.trader.kata.framework.CBOE;
-import com.turleylabs.algo.trader.kata.framework.Holding;
-import com.turleylabs.algo.trader.kata.framework.SimpleMovingAverage;
-import com.turleylabs.algo.trader.kata.framework.Slice;
+import com.turleylabs.algo.trader.kata.framework.*;
+import org.lambda.actions.Action2;
+import org.lambda.functions.Function1;
 
 import java.time.LocalDate;
 
 public class RefactorMeAlgorithm extends BaseAlgorithm {
 
+    public static final double VIX_THRESHOLD = 25.0;
     String symbol = "TQQQ";
-    SimpleMovingAverage movingAverage200;
-    SimpleMovingAverage movingAverage50;
-    SimpleMovingAverage movingAverage21;
-    SimpleMovingAverage movingAverage10;
+    public SimpleMovingAverage movingAverage200;
+    public SimpleMovingAverage movingAverage50;
+    public SimpleMovingAverage movingAverage21;
+    public SimpleMovingAverage movingAverage10;
     double previousMovingAverage50;
     double previousMovingAverage21;
     double previousMovingAverage10;
@@ -37,71 +36,142 @@ public class RefactorMeAlgorithm extends BaseAlgorithm {
     }
 
     protected void onData(Slice data) {
+        LocalDate date = getDate();
+        onData2(data, date, s -> data.get(s));
+    }
+
+    public void onData2(Slice data, LocalDate date, Function1<String, Bar> getBar) {
+
         if (data.getCBOE("VIX") != null) {
             lastVix = data.getCBOE("VIX");
         }
-        if (previous == getDate()) {
+        if (previous == date) {
             return;
         }
 
         if (!movingAverage200.isReady()) {
             return;
         }
-
-        if (data.get(symbol) == null) {
+        Bar bar = getBar.call(symbol);
+        if (bar == null) {
             this.log(String.format("No data for symbol %s", symbol));
             return;
         }
+
         if (tookProfits) {
-            if (data.get(symbol).getPrice() < movingAverage10.getValue()) {
+            if (bar.getPrice() < movingAverage10.getValue()) {
                 tookProfits = false;
             }
         } else if (portfolio.getOrDefault(symbol, Holding.Default).getQuantity() == 0) {
 
-            if (data.get(symbol).getPrice() > movingAverage10.getValue()
-                    && movingAverage10.getValue() > movingAverage21.getValue()
-                    && movingAverage10.getValue() > previousMovingAverage10
-                    && movingAverage21.getValue() > previousMovingAverage21
-                    && lastVix.getClose() < 25.0
-                    && !(data.get(symbol).getPrice() >= (movingAverage50.getValue() * 1.15) && data.get(symbol).getPrice() >= (movingAverage200.getValue() * 1.40))
-                    && (data.get(symbol).getPrice() - movingAverage10.getValue()) / movingAverage10.getValue() < 0.07) {
-                this.log(String.format("Buy %s Vix %.4f. above 10 MA %.4f", symbol, lastVix.getClose(), (data.get(symbol).getPrice() - movingAverage10.getValue()) / movingAverage10.getValue()));
-                double amount = 1.0;
-                this.setHoldings(symbol, amount);
-
-                boughtBelow50 = data.get(symbol).getPrice() < movingAverage50.getValue();
-            }
+            perhapsBuyStuff(bar, lastVix.getClose(), previousMovingAverage10, previousMovingAverage21, this::setHoldings);
         } else {
-            double change = (data.get(symbol).getPrice() - portfolio.get(symbol).getAveragePrice()) / portfolio.get(symbol).getAveragePrice();
+            sellStuff(bar);
+        }
 
-            if (data.get(symbol).getPrice() < (movingAverage50.getValue() * .93) && !boughtBelow50) {
-                this.log(String.format("Sell %s loss of 50 day. Gain %.4f. Vix %.4f", symbol, change, lastVix.getClose()));
+        previous = date;
+        previousMovingAverage50 = movingAverage50.getValue();
+        previousMovingAverage21 = movingAverage21.getValue();
+        previousMovingAverage10 = movingAverage10.getValue();
+        previousPrice = bar.getPrice();
+    }
+
+    public Buy perhapsBuyStuff(Bar bar, double vix, double previousMovingAverage10, double previousMovingAverage21, Action2<String, Double> buyAction) {
+        Buy buy = null;
+        double price = bar.getPrice();
+        double avg10 = movingAverage10.getValue();
+        double avg50 = movingAverage50.getValue();
+        double avg200 = movingAverage200.getValue();
+        double avg21 = movingAverage21.getValue();
+        if (isPriceTrendingUp(previousMovingAverage10, previousMovingAverage21, price, avg10, avg21)
+                && vix < VIX_THRESHOLD
+                && notAnOutliner(price, avg10, avg50, avg200)) {
+
+            this.log(
+                    String.format("Buy %s Vix %.4f. above 10 MA %.4f",
+                            symbol,
+                            vix,
+                            (price - avg10) / avg10));
+
+            double amount = 1.0;
+            buyAction.call(symbol, amount);
+
+            boolean boughtBelow50 = price < movingAverage50.getValue();
+
+            buy = new Buy(symbol, amount, boughtBelow50);
+            this.boughtBelow50 = boughtBelow50;
+        }
+        return buy;
+    }
+
+    private boolean notAnOutliner(double price, double avg10, double avg50, double avg200) {
+        return !isPriceAbove15PercentOfMovingAvg50And40PercentMoving200(price, avg50, avg200)
+                && isAverageTenOkay(price, avg10);
+    }
+
+    private boolean isPriceTrendingUp(double previousMovingAverage10, double previousMovingAverage21, double price, double avg10, double avg21) {
+        boolean isAverageBetweenPreviousAndThePrice10 = previousMovingAverage10 < avg10 && avg10 < price;
+        boolean isTenDayAverageAboveThePreviousAverages = previousMovingAverage21 < avg21 && avg21 < avg10;
+        return isAverageBetweenPreviousAndThePrice10 && isTenDayAverageAboveThePreviousAverages;
+    }
+
+    private boolean isAverageTenOkay(double price, double avg10) {
+        double differece = price - avg10;
+        double result = differece / avg10;
+        return result < 0.07;
+    }
+
+    private static boolean isPriceAbove15PercentOfMovingAvg50And40PercentMoving200(double price, double avg50, double avg200) {
+        boolean above15PercentOfMovAvg50 = (avg50 * 1.15) <= price;
+        boolean above40PercentMovAvg200 = (avg200 * 1.40) <= price;
+        boolean result = above15PercentOfMovAvg50 && above40PercentMovAvg200;
+        return result;
+    }
+
+    private void sellStuff(Bar bar) {
+        double change = (bar.getPrice() - portfolio.get(symbol).getAveragePrice()) / portfolio.get(symbol).getAveragePrice();
+
+        if (bar.getPrice() < (movingAverage50.getValue() * .93) && !boughtBelow50) {
+            this.log(String.format("Sell %s loss of 50 day. Gain %.4f. Vix %.4f", symbol, change, lastVix.getClose()));
+            this.liquidate(symbol);
+        } else {
+            if (lastVix.getClose() > 28.0) {
+                this.log(String.format("Sell %s high volatility. Gain %.4f. Vix %.4f", symbol, change, lastVix.getClose()));
                 this.liquidate(symbol);
             } else {
-                if (lastVix.getClose() > 28.0) {
-                    this.log(String.format("Sell %s high volatility. Gain %.4f. Vix %.4f", symbol, change, lastVix.getClose()));
+                if (movingAverage10.getValue() < 0.97 * movingAverage21.getValue()) {
+                    this.log(String.format("Sell %s 10 day below 21 day. Gain %.4f. Vix %.4f", symbol, change, lastVix.getClose()));
                     this.liquidate(symbol);
                 } else {
-                    if (movingAverage10.getValue() < 0.97 * movingAverage21.getValue()) {
-                        this.log(String.format("Sell %s 10 day below 21 day. Gain %.4f. Vix %.4f", symbol, change, lastVix.getClose()));
+                    if (bar.getPrice() >= (movingAverage50.getValue() * 1.15) && bar.getPrice() >= (movingAverage200.getValue() * 1.40)) {
+                        this.log(String.format("Sell %s taking profits. Gain %.4f. Vix %.4f", symbol, change, lastVix.getClose()));
                         this.liquidate(symbol);
-                    } else {
-                        if (data.get(symbol).getPrice() >= (movingAverage50.getValue() * 1.15) && data.get(symbol).getPrice() >= (movingAverage200.getValue() * 1.40)) {
-                            this.log(String.format("Sell %s taking profits. Gain %.4f. Vix %.4f", symbol, change, lastVix.getClose()));
-                            this.liquidate(symbol);
-                            tookProfits = true;
-                        }
+                        tookProfits = true;
                     }
                 }
             }
         }
-
-
-        previous = getDate();
-        previousMovingAverage50 = movingAverage50.getValue();
-        previousMovingAverage21 = movingAverage21.getValue();
-        previousMovingAverage10 = movingAverage10.getValue();
-        previousPrice = data.get(symbol).getPrice();
     }
 
+    public static class Buy {
+
+        private final String symbol;
+        private final double amount;
+        private final boolean boughtBelow50;
+
+        public Buy(String symbol, double amount, boolean boughtBelow50) {
+            this.symbol = symbol;
+            this.amount = amount;
+            this.boughtBelow50 = boughtBelow50;
+        }
+
+        @Override
+        public String toString() {
+            return "Buy{" +
+                    "symbol='" + symbol + '\'' +
+                    ", amount=" + amount +
+                    ", boughtBelow50=" + boughtBelow50 +
+                    '}';
+        }
+    }
 }
--- src/test/java/com/turleylabs/algo/trader/kata/AlgorithmTest.java
+++ src/test/java/com/turleylabs/algo/trader/kata/AlgorithmTest.java
@@ -1,28 +1,68 @@
 package com.turleylabs.algo.trader.kata;
 
-import org.approvaltests.Approvals;
-import org.junit.jupiter.api.Test;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.PrintStream;
-import java.nio.charset.StandardCharsets;
+import com.turleylabs.algo.trader.kata.framework.Bar;
+import com.turleylabs.algo.trader.kata.framework.SimpleMovingAverage;
+import org.approvaltests.combinations.CombinationApprovals;
+import org.approvaltests.reporters.ClipboardReporter;
+import org.approvaltests.reporters.UseReporter;
+import org.junit.Test;
 
 public class AlgorithmTest {
 
-    @Test
-    void algorithmExecutesTrades() {
+    /*
+        @Disabled("too slow")
+        @Test
+        void algorithmExecutesTrades() {
 
-        try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
-            System.setOut(new PrintStream(baos, true, StandardCharsets.UTF_8));
+            try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+                System.setOut(new PrintStream(baos, true, StandardCharsets.UTF_8));
 
-            RefactorMeAlgorithm refactorAlgorithm = new RefactorMeAlgorithm();
-            refactorAlgorithm.run();
+                RefactorMeAlgorithm refactorAlgorithm = new RefactorMeAlgorithm();
+                refactorAlgorithm.run();
 
-            Approvals.verify(baos.toString());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+                Approvals.verify(baos.toString());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
+    */
+    @UseReporter({ClipboardReporter.class})
+    //@Test
+    public void test_on_data_combinations() {
+        var prices = new Double[] {0.06, 0.4, 1.0, 1.4, 2.4, 3.4};
+        var diffs = new Integer[] {1, -1};
+        var movingAverages = new Double[] {1.0, 2.0, 3.0};
+
+        CombinationApprovals.verifyAllCombinations(
+                AlgorithmTest::executeBuy, prices, diffs, movingAverages, movingAverages, movingAverages, movingAverages, movingAverages, movingAverages);
     }
 
+    @Test
+    public void test_almost_there() {
+        executeBuy(3.21, -1, 3.0, 2.9, 3.025, 3.1, 2.9, 2.8);
+    }
+
+    public static RefactorMeAlgorithm.Buy executeBuy(
+            Double price, Integer differenceFromVixThreshold, Double movingAvg10, Double movingAvg21, Double movingAvg50, Double movingAvg200, Double previousMovingAverage10,
+            Double previousMovingAverage21) {
+        RefactorMeAlgorithm refactorAlgorithm = new RefactorMeAlgorithm();
+
+        refactorAlgorithm.movingAverage10 = createMovingAverage(movingAvg10);
+        refactorAlgorithm.movingAverage21 = createMovingAverage(movingAvg21);
+        refactorAlgorithm.movingAverage50 = createMovingAverage(movingAvg50);
+        refactorAlgorithm.movingAverage200 = createMovingAverage(movingAvg200);
+
+        //when
+        return refactorAlgorithm.perhapsBuyStuff(new Bar(price), RefactorMeAlgorithm.VIX_THRESHOLD + differenceFromVixThreshold, previousMovingAverage10,
+                previousMovingAverage21, (a, b) -> {
+                });
+    }
+
+    private static SimpleMovingAverage createMovingAverage(double... input) {
+        SimpleMovingAverage simpleMovingAverage = new SimpleMovingAverage("anySymbol", input.length);
+        for (double i : input) {
+            simpleMovingAverage.addData(i);
+        }
+        return simpleMovingAverage;
+    }
 }
